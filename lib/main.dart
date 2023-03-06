aa45d4a9cc9b9464887cfdd9e41627af8eba1af8
Arif Ghiffari <arifghiffari@192.168.0.15>
2023-03-06 19:25:41 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SimpleCalculator(),
+    );
+  }
+}
+
+class SimpleCalculator extends StatefulWidget {
+  const SimpleCalculator({super.key});
+
+  @override
+  State<SimpleCalculator> createState() => _SimpleCalculatorState();
+}
+
+class _SimpleCalculatorState extends State<SimpleCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Simple Calculator')),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text(
+              '0',
+              style: TextStyle(fontSize: 38.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+            child: Text(
+              '0',
+              style: TextStyle(fontSize: 48.0),
+            ),
+          ),
+          Expanded(child: child)
+        ],
+      ),
+    );
+  }
+}
